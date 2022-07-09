83f028523f76d82c4c868eca619b1eb199400d9d
junghochoi <dchoi315@gmail.com>
2022-07-09 17:15:23 -0400

    created simple frontend

---
+++ poly-deposit-fe/.history/pages/index_20220709164552.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+import {
+  Container
+} from '@chakra-ui/react';
+
+const Home: NextPage = () => {
+  return (
+    <
+  )
+}
+
+export default Home
---
+++ poly-deposit-fe/.history/pages/index_20220709164601.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+import {
+  Container
+} from '@chakra-ui/react'
+
+const Home: NextPage = () => {
+  return (
+    <Container>
+      Hello
+    </Container>
+  )
+}
+
+export default Home
